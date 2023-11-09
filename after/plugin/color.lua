e08e4e62b24fa1992fbecf08c2d20e869abe3301
Chris <4070267+Chris-Holdt@users.noreply.github.com>
2023-11-09 14:24:01 +0000

    Switched to spacerain being the default theme

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,10 +1,10 @@
 function MakePrettyColours(colour)
-	colour = colour or "catppuccin-latte"
-	vim.cmd.colorscheme(colour)
+  colour = colour or "spacerain"
+  vim.cmd.colorscheme(colour)
 
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 MakePrettyColours()
